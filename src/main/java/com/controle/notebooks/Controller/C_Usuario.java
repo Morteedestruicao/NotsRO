ea161513f2badcb521f189bb013bf3cfeb7c8ed9
Zanottex <gustavozanotto119@gmail.com>
2023-09-21 17:36:40 -0300

    Dia 21/09/2023

--- src/main/java/com/controle/notebooks/Controller/C_Usuario.java
+++ src/main/java/com/controle/notebooks/Controller/C_Usuario.java
@@ -1,5 +1,6 @@
 package com.controle.notebooks.Controller;
 
+import com.controle.notebooks.Service.S_Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,16 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class C_Usuario {
 
     @GetMapping("/")
-    public String getCadastro(){
-       return "usuario/cadastro";
+    public String getCadastro() {
+        return "usuario/cadastro";
     }
 
-    @PostMapping("/")
+    @PostMapping("/cadastro")
     public String cadatrarUsuario(@RequestParam("nome") String nome,
                                   @RequestParam("email") String email,
                                   @RequestParam("matricula") String matricula,
                                   @RequestParam("cargo") String cargo
-    ){
-        return null;
+    ) {
+        S_Usuario.cadastrarUsuario(nome, cargo, matricula, email);
+        return "usuario/cadastro";
     }
 }
---
+++ src/main/java/com/controle/notebooks/Model/M_Usuario.java
@@ -0,0 +1,78 @@
+package com.controle.notebooks.Model;
+
+
+import jakarta.persistence.*;
+import org.apache.logging.log4j.message.StringFormattedMessage;
+
+@Entity
+@Table(name = "usuarios")
+public class M_Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Long id_cargo;
+    private Long matricula;
+    private String email;
+
+//    public M_Usuario(Long id, String nome, Long id_cargo, Long matricula, String email, String senha) {
+//        this.id = id;
+//        this.nome = nome;
+//        this.id_cargo = id_cargo;
+//        this.matricula = matricula;
+//        this.email = email;
+//        this.senha = senha;
+//    }
+
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(Long id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+}
